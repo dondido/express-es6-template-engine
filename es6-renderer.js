93cf76b06dead26f658a69e3085aca724a35d27a
Dan Peddle <dazld@users.noreply.github.com>
2016-01-19 18:21:30 +0100

    reject on error, not just console log

--- es6-renderer.js
+++ es6-renderer.js
@@ -10,7 +10,7 @@ module.exports = (function(options) { // define the template engine
       (resolve, reject) => fs.readFile(
         filePath,
         'utf-8',
-        (err, content) => err ? console.log(new Error(err)) : resolve(content)
+        (err, content) => err ? reject(new Error(err))  : resolve(content)
       )
     );
   this.render = (filePath, dict, callback) => {
