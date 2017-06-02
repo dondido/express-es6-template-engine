034806cd64ce2e4fd32c57a851bd15ea90b1ec19
dondido <dian_t_d@yahoo.co.uk>
2017-06-02 09:09:13 +0100

    Update es6-renderer.js

--- es6-renderer.js
+++ es6-renderer.js
@@ -1,64 +1,62 @@
-const fs = require('fs'), // this engine requires the fs module
-  /* jshint ignore:start */
-  interpolate = (content, localsKeys, localsValues) => new Function(
-    ...localsKeys,
-    'return `' + content + '`;'
-  )(...localsValues),
-  setPath = (views, ref, ext) => ref.endsWith(ext) ? ref : views  + '/' + ref + ext,
-  /* jshint ignore:end */
-  readPartial = path => {
-    const findFile = (resolve, reject) => {
-      const getContent = (err, content) => err ? reject(new Error(err)) : resolve(content);
-      fs.readFile(path, 'utf-8', getContent);
-    };
-    return new Promise(findFile);
+const fs = require('fs'); // this engine requires the fs module
+/* jshint ignore:start */
+const compile = (content, $ = '$') => Function($, 'return `' + content + '`;');
+/* jshint ignore:end */
+const setPath = (views, ref, ext) => ref.endsWith(ext) ? ref : views  + '/' + ref + ext;
+const getPartial = (path, cb = 'resolveNeutral') => {
+  const findFile = function(resolve, reject) {
+    this.resolveNeutral = (err, content) => err ? reject(new Error(err)) : resolve(content);
+    this.resolvePositive = (err, content) => resolve(err || content);
+    fs.readFile(path, 'utf-8', this[cb]);
   };
+  return new Promise(findFile);
+};
     
-module.exports = (path, options, callback) => {
-  const {locals = {}, partials = {}, template, settings} = options,
-    compile = (err, content) => {
-      if(err) {
-        return callback(new Error(err));
-      }
-      const localsKeys = Object.keys(locals),
-        localsValues = localsKeys.map(i => locals[i]),
-        partialsKeys = Object.keys(partials);
-      if(partialsKeys.length) {
-        const applySettings = partialKey => {
-          const ext = '.' + settings['view engine']; 
-          if(typeof settings.views === 'string') {
-            return i => readPartial(setPath(settings.views, partials[i], ext));
-          }
-          const views = settings.views.slice();
-          const requestFiles = i => {
-            const searchFile = (resolve, reject) => {
-              const getContent = (err, content) => {
-                if (err) {
-                  return views.length ? repeatSearch() : reject(new Error(err));
-                }
-                resolve(content);
-              };
-              const repeatSearch = () => fs.readFile(setPath(views.shift(), partials[i], ext), 'utf-8', getContent);
-              repeatSearch();
-            }
-            return new Promise(searchFile);
-          };
-          return requestFiles;
-        };
-        const setPartial = settings ? applySettings() : i => readPartial(partials[i]);
-        localsKeys.push(...partialsKeys);
-        return Promise.all(partialsKeys.map(setPartial))
-          .then(values => {
-            const valTempList = localsValues.concat(values);
-            localsValues.push(...values.map(i => interpolate(i, localsKeys, valTempList)));
-            return callback(null, interpolate(content, localsKeys, localsValues));
-          })
-          .catch(err => callback(err));
-      }
-      return callback(null, interpolate(content, localsKeys, localsValues));
-    };
-  if (template) {
-    return compile(null, path);
+module.exports = (path, options, cb) => {
+  if(options === undefined || typeof options === 'string') {
+    return compile(path);
   }
-  fs.readFile(path, 'utf-8', compile);
+  const render = cb || ((err, content) => err || content);
+  const {locals = {}, partials = {}, settings} = options;
+  
+  const assign = (err, content) => {
+    if(err) {
+      return render(new Error(err));
+    }
+    const localsKeys = Object.keys(locals);
+    const localsValues = localsKeys.map(i => locals[i]);
+    const partialsKeys = Object.keys(partials);
+    const compilePartials = values => {
+      const valTempList = localsValues.concat(values);
+      localsValues.push(...values.map(i => compile(i, localsKeys)(...valTempList)));
+      return render(null, compile(content, localsKeys)(...localsValues));
+    };
+    if(partialsKeys.length) {
+      const applySettings = () => {
+        const ext = '.' + settings['view engine'];
+        if(typeof settings.views === 'string') {
+          return i => getPartial(setPath(settings.views, partials[i], ext));
+        }
+        return i => {
+          const getFile = view => getPartial(setPath(view, partials[i], ext), 'resolvePositive');
+          const getFirst = value => typeof value === 'string';
+          const searchFile = (resolve, reject) => {
+            const getContent = values => resolve(values.find(getFirst));
+            Promise.all(settings.views.map(getFile)).then(getContent);
+          };
+          return new Promise(searchFile);
+        };
+      };
+      const setPartial = settings ? applySettings() : i => getPartial(partials[i]);
+      localsKeys.push(...partialsKeys);
+      return Promise.all(partialsKeys.map(setPartial))
+        .then(compilePartials)
+        .catch(err => render(err));
+    }
+    return render(null, compile(content, localsKeys)(...localsValues));
+  };
+  if (cb === undefined) {
+    return assign(null, path);
+  }
+  fs.readFile(path, 'utf-8', assign);
 };
